3dae74b99b105d4c97f8c8cd0e6069af2bbae5dc
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-06-19 18:27:42 +0200

    Fájlrenszer szervezéses feladatok kódja.

---
+++ nodejs-alapjai-fs-modul/index.js
@@ -0,0 +1,47 @@
+const { mkdir, writeFile } = require('fs').promises;
+const { createReadStream, createWriteStream, readFile, unlink } = require('fs')
+const { createGzip } = require('zlib');
+
+const folderArray = ["controllers", "routers", "views"]
+const fileArray = [
+    './controllers/site.controller.js',
+    './routers/site.router.js',
+    './views/index.html',
+    './app.js'
+]
+
+const createFolders = folderArray => {
+    return folderArray.map(folder => mkdir(folder))
+}
+
+const createFiles = fileArray => {
+    return fileArray.map(file => writeFile(file, ``))
+}
+
+createFolders(folderArray);
+createFiles(fileArray);
+
+const path = require('path');
+const archiveFileApplication = (file) => {
+
+    const filePath = `${file}`
+    const copyPath = path.join(__dirname, `${file}.bak`)
+    
+    const writeableStream = createWriteStream(`${file}.bak`)
+    const createCompressedFile = createWriteStream(`${file}.gz`)
+
+    const readableStream = createReadStream(file, {
+        encoding: 'utf-8',
+        highWaterMark: 199
+    })
+    readableStream.pipe(writeableStream)
+    readableStream
+        .pipe(createGzip())
+        .pipe(createCompressedFile);
+
+    writeableStream.on('finish', () => {    
+        unlink(filePath)
+        unlink(copyPath)
+    }) 
+    archiveFileApplication('./archive.txt')
+}
