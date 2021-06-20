2c0d789c60e1690007b1dd15769a248506fa1944
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-06-20 17:34:57 +0200

    Nem működik jól.

---
+++ nodejs-alapjai-events/index.js
@@ -0,0 +1,33 @@
+const Logger = require('./logger')
+const {createReadStream, createWriteStream } = require('fs')
+
+const loggerExample = new Logger()
+const path = './arbitrary'
+
+const readableStream = createReadStream(`${path}Copy.txt`, { 
+    encoding: 'utf-8', 
+    highWaterMark: 822 
+});
+
+const writeableStream = createWriteStream(`${path}Copy.txt`)
+readableStream
+    .pipe(writeableStream)
+
+const capitalizeLetter = string => {
+    string.charAt(0).toLocaleUpperCase() + string.slice(1)
+}
+
+readableStream.on('capitalizer', (word) => {
+    try {
+        const transformLetters =  word.split(' ')
+            .map(letter => capitalizeLetter(letter))
+            .join(' ')    
+        console.log(transformLetters);
+        loggerExample.success('File transform successful.')
+    } catch (error) {
+        loggerExample.error(error.message)
+    }
+})
+
+loggerExample.emit('capitalizer', readableStream);
+
--- nodejs-alapjai-events/logger.js
+++ nodejs-alapjai-events/logger.js
@@ -1,13 +1,12 @@
 const EventEmitter = require('events')
 
-class Logger extends Evenmitter {
-    constructor() {}
-
+class Logger extends EventEmitter {
     error(errorMessage) {
         console.log('\x1b[31m', errorMessage);
     }
     success(successMessage) {
          console.log('\x1b[32m', successMessage);
     }
-
 }
+
+module.exports = Logger 
